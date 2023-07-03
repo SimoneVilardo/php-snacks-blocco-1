91e59bb27169146df3baa89b59da2e91fc1fd369
SimoneVilardo <simovila95@gmail.com>
2023-07-03 15:31:25 +0200

    snack 3 completato

---
+++ snack-3/index.php
@@ -0,0 +1,67 @@
+<?php
+    $posts = [
+
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
+    
+    foreach($posts as $data => $list){
+        echo "<hr>";
+        echo "Data:".$data."<br>";
+        echo "<hr>";
+
+        foreach($list as $post){
+            echo "Post:".$post['title']."<br>";
+            echo "Author:".$post['author']."<br>";
+            echo "Text:".$post['text']."<br>";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Snacks Blocco 1</title>
+</head>
+    <body>
+        <div>
+            
+        </div>
+    </body>
+</html>
